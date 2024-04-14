4f296edb1463c88a67c7b41e77242425fbed68df
sdvuuv <sedovann05.irk@gmail.com>
2024-04-15 00:34:19 +0800

    hw 13

--- Src/Logics/storage_service.py
+++ Src/Logics/storage_service.py
@@ -7,19 +7,21 @@ from Src.Models.receipe_model import receipe_model
 from Src.Models.storage_model import storage_model
 from Src.Models.receipe_row_model import receipe_row_model
 from Src.Storage.storage import storage
-
 from datetime import datetime
+from Src.settings import settings
 import json
+
 
 class storage_service:
     __data = []
-    
+    __options = None
+
     def __init__(self, data: list) -> None:
         if len(data) == 0:
             raise argument_exception("Некорректно переданы параметры!")
-        
+
         self.__data = data
-        
+
     def __processing(self, data: list) -> list:
         """
             Сформировать обороты
@@ -31,19 +33,18 @@ class storage_service:
         """
         if len(data) == 0:
             raise argument_exception("Некорректно переданы параметры!")
-        
-        # Подобрать процессинг    
+
+        # Подобрать процессинг
         key_turn = process_factory.turn_key()
-        processing = process_factory().create( key_turn  )
-    
+        processing = process_factory().create(key_turn)
+
         # Обороты
-        turns =  processing().process( data )
+        turns = processing().process(data)
         return turns
-            
-        
-    # Набор основных методов    
-        
-    def create_turns(self, start_period: datetime, stop_period:datetime ) -> list:
+
+    # Набор основных методов
+
+    def create_turns(self, start_period: datetime, stop_period: datetime) -> list:
         """
             Получить обороты за период
         Args:
@@ -55,18 +56,22 @@ class storage_service:
         """
         exception_proxy.validate(start_period, datetime)
         exception_proxy.validate(stop_period, datetime)
-        
+        block_period = self.__options.block_period
         if start_period > stop_period:
             raise argument_exception("Некорректно переданы параметры!")
-        
-        # Фильтруем      
-        prototype = storage_prototype(  self.__data )  
-        filter = prototype.filter_by_period( start_period, stop_period)
-        
-        return self.__processing( filter. data )
-            
-        
-    def create_turns_by_nomenclature(self, start_period: datetime, stop_period: datetime, nomenclature: nomenclature_model) -> list:
+        if block_period < start_period:
+            raise argument_exception("Некорректно переданы параметры!")
+        if block_period < stop_period:
+            stop_period = block_period
+
+        # Фильтруем
+        prototype = storage_prototype(self.__data)
+        filter = prototype.filter_by_period(start_period, stop_period)
+
+        return self.__processing(filter.data)
+
+    def create_turns_by_nomenclature(self, start_period: datetime, stop_period: datetime,
+                                     nomenclature: nomenclature_model) -> list:
         """
             Получить обороты за период по конкретной номенклатуры
         Args:
@@ -80,19 +85,22 @@ class storage_service:
         exception_proxy.validate(start_period, datetime)
         exception_proxy.validate(stop_period, datetime)
         exception_proxy.validate(nomenclature, nomenclature_model)
-        
+        block_period = self.__options.block_period
         if start_period > stop_period:
             raise argument_exception("Некорректно переданы параметры!")
-        
-        # Фильтруем      
-        prototype = storage_prototype(  self.__data )  
-        filter = prototype.filter_by_period( start_period, stop_period)
-        filter = filter.filter_by_nomenclature( nomenclature )
+        if block_period < start_period:
+            raise argument_exception("Некорректно переданы параметры!")
+        if block_period < stop_period:
+            stop_period = block_period
+        # Фильтруем
+        prototype = storage_prototype(self.__data)
+        filter = prototype.filter_by_period(start_period, stop_period)
+        filter = filter.filter_by_nomenclature(nomenclature)
         if not filter.is_empty:
             raise operation_exception(f"Невозможно сформировать обороты по указанным данных: {filter.error}")
-            
-        return self.__processing( filter. data )    
-    
+
+        return self.__processing(filter.data)
+
     def create_turns_only_nomenclature(self, nomenclature: nomenclature_model) -> list:
         """
             Получить обороты по номенклатуре
@@ -103,13 +111,13 @@ class storage_service:
             list: Обороты
         """
         exception_proxy.validate(nomenclature, nomenclature_model)
-        prototype = storage_prototype(  self.__data )  
-        filter = prototype.filter_by_nomenclature( nomenclature )
+        prototype = storage_prototype(self.__data)
+        filter = prototype.filter_by_nomenclature(nomenclature)
         if not filter.is_empty:
             raise operation_exception(f"Невозможно сформировать обороты по указанным данных: {filter.error}")
-         
-        return self.__processing( filter. data )   
-    
+
+        return self.__processing(filter.data)
+
     def create_turns_by_receipt(self, receipt: receipe_model) -> list:
         """
             Сформировать обороты по указанному рецепту
@@ -120,24 +128,23 @@ class storage_service:
             list: _description_
         """
         exception_proxy.validate(receipt, receipe_model)
-        
+
         if len(receipt.consist) == 0:
             raise operation_exception("Переданный рецепт некорректный. Не содержит в себе список номенклатуры!")
-        
+
         # Отфильтровать по рецепту
         transactions = []
-        filter =  storage_prototype(  self.__data )
+        filter = storage_prototype(self.__data)
         for item in receipt.rows():
-            filter =  filter.filter_by_nomenclature( item.nomenclature )
+            filter = filter.filter_by_nomenclature(item.nomenclature)
             if filter.is_empty:
                 for transaction in filter.data:
-                    transactions.append( transaction )
-                    
-            filter.data = self.__data        
-            
-        return self.__processing( transactions )     
-            
-    
+                    transactions.append(transaction)
+
+            filter.data = self.__data
+
+        return self.__processing(transactions)
+
     def build_debits_by_receipt(self, receipt: receipe_model) -> list:
         """
             Сформировать проводки списания по рецепту
@@ -148,38 +155,73 @@ class storage_service:
             list: _description_
         """
         exception_proxy.validate(receipt, receipe_model)
-        
+
         if len(receipt.consist) == 0:
             raise operation_exception("Переданный рецепт некорректный. Не содержит в себе список номенклатуры!")
-        
+
         turns = self.create_turns_by_receipt(receipt)
         if len(turns) <= 0:
             raise operation_exception("По указанному рецепту не найдеты обороты!")
-        
+
         if len(receipt.rows()) > len(turns):
-            raise operation_exception("Невозможно сформировать список транзакций для списания т.к. нет достаточно остатков!")
-        
+            raise operation_exception(
+                "Невозможно сформировать список транзакций для списания т.к. нет достаточно остатков!")
+
         # Формируем список проводок на списание
-        processing = process_factory().create( process_factory.debit_key() )
-        transactions = processing().process( receipt.rows() )
+        processing = process_factory().create(process_factory.debit_key())
+        transactions = processing().process(receipt.rows())
         key = storage.storage_transaction_key()
-        
-        data = storage().data[ key ]
+
+        data = storage().data[key]
         for transaction in transactions:
-            data.append ( transaction )
-    
-    # Набор основных методов        
-    
+            data.append(transaction)
+
+    @property
+    def options(self):
+        return self.__options
+
+    @options.setter
+    def options(self, value: settings):
+        if not isinstance(value, settings):
+            raise argument_exception("неверный аргумент")
+        self.__options = value
+
+    def create_blocked_turns(self) -> list:
+        """
+            Получить обороты за период
+        Args:
+            start_period (datetime): Начало
+            stop_period (datetime): Окончание
+
+        Returns:
+            list: обороты за период
+        """
+        start_period = datetime(1999, 1, 1)
+        exception_proxy.validate(start_period, datetime)
+        stop_period = self.__options.block_period
+        exception_proxy.validate(stop_period, datetime)
+
+        if start_period > stop_period:
+            raise argument_exception("Некорректно переданы параметры!")
+
+        # Фильтруем
+        prototype = storage_prototype(self.__data)
+        filter = prototype.filter_by_period(start_period, stop_period)
+
+        return self.__processing(filter.data)
+
+    # Набор основных методов
+
     def data(self) -> list:
         """
             Получить отфильтрованные данные
         Returns:
             list: _description_
         """
-        return self.__data    
-        
-    @staticmethod        
-    def create_response( data: list, app):
+        return self.__data
+
+    @staticmethod
+    def create_response(data: list, app):
         """"
             Сформировать данные для сервера
         """
@@ -187,14 +229,14 @@ class storage_service:
             raise argument_exception("Некорректно переданы параметры!")
 
         # Преоброзование
-        data = convert_factory().serialize( data )
-        json_text = json.dumps( data, sort_keys = True, indent = 4,  ensure_ascii = False)  
-   
-        # Подготовить ответ    
+        data = convert_factory().serialize(data)
+        json_text = json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
+
+        # Подготовить ответ
         result = app.response_class(
-            response = f"{json_text}",
-            status = 200,
-            mimetype = "application/json; charset=utf-8"
+            response=f"{json_text}",
+            status=200,
+            mimetype="application/json; charset=utf-8"
         )
-        
+
         return result
